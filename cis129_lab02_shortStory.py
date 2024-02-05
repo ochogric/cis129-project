1d0f8a82a90142059e83094486d455c2a4c71330
ochogric <158656986+ochogric@users.noreply.github.com>
2024-02-05 16:32:43 -0700

    Update cis129_lab02_shortStory.py
    I gave a short story about my first class

--- cis129_lab02_shortStory.py
+++ cis129_lab02_shortStory.py
@@ -1,1 +1,3 @@
-print('Hello, World!'
+# Author: Ricardo Ochoa
+# My first Python lab in my CIS129 course
+#Today I went to my first class of the week but the teacher wasnt there. So one student volunteered to teach the class for the day. He was the smartest student in class so I was pretty sure what he was talking about. When the class end the teacher that was suppose to teach was hiding in the ceiling and it was all planned and he was happy for what happen so he gave us an A in his class.
